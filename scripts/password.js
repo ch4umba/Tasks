cb1adf043674b1cfe67e95fc6c5722610bbbb6f1
Denis <cool84321@gmail.com>
2022-02-05 02:36:56 +0700

    First, second and third tasks

---
+++ scripts/password.js
@@ -0,0 +1,12 @@
+function setVisibility(target){
+	var input = document.getElementById('password-input');
+	if (input.getAttribute('type') == 'password') {
+		target.classList.add('view');
+		input.setAttribute('type', 'text');
+	} 
+	else {
+		target.classList.remove('view');
+		input.setAttribute('type', 'password');
+	}
+	return false;
+}
